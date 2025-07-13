d93d418f24f5bfca476ed54061161cd42500ce4f
Viet Vu <jooservices@gmail.com>
2025-07-13 22:47:41 +0700

    init

--- lib/cdk-stack.ts
+++ lib/cdk-stack.ts
@@ -1,12 +1,24 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
 
 export class CdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
     // The code that defines your stack goes here
+
+    // Lambda function resource
+    const helloLambda = new lambda.Function(this, 'HelloLambda', {
+      runtime: lambda.Runtime.NODEJS_22_X,
+      handler: 'index.handler',
+      code: lambda.Code.fromInline(`
+        exports.handler = async function(event) {
+          return { statusCode: 200, body: 'Hello from Lambda!' };
+        };
+      `),
+    });
 
     // example resource
     // const queue = new sqs.Queue(this, 'CdkQueue', {
