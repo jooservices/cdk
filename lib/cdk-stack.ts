35870cde0b0d2b98498c60edbb05773d8300a54d
Viet Vu <jooservices@gmail.com>
2025-07-13 22:54:25 +0700

    Adding Python function with VPC

--- lib/cdk-stack.ts
+++ lib/cdk-stack.ts
@@ -2,6 +2,7 @@ import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as ec2 from 'aws-cdk-lib/aws-ec2';
 
 export class CdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
@@ -18,6 +19,23 @@ export class CdkStack extends cdk.Stack {
           return { statusCode: 200, body: 'Hello from Lambda!' };
         };
       `),
+    });
+
+    // VPC for the Python Lambda function
+    const vpc = new ec2.Vpc(this, 'LambdaVpc', {
+      maxAzs: 2, // Default is all AZs in region
+    });
+
+    // Python Lambda function in the VPC
+    const pythonLambda = new lambda.Function(this, 'PythonLambda', {
+      runtime: lambda.Runtime.PYTHON_3_12,
+      handler: 'index.handler',
+      code: lambda.Code.fromInline(`
+import json
+def handler(event, context):
+    return { 'statusCode': 200, 'body': json.dumps('Hello from Python Lambda!') }
+      `),
+      vpc: vpc,
     });
 
     // example resource
